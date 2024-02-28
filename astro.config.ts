a61b96dc46848b8e2cba0707378d8c0adc58082b
nvkhuy <huynvk@huy.local>
2024-02-28 22:13:22 +0700

    fix reponsive

--- astro.config.ts
+++ astro.config.ts
@@ -1,40 +1,50 @@
-import { defineConfig } from "astro/config";
+import {defineConfig} from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
-import { SITE } from "./src/config";
+import {mermaid} from "./src/plugins/mermaid"
+import {SITE} from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE.website,
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-    sitemap(),
-  ],
-  markdown: {
-    remarkPlugins: [
-      remarkToc,
-      [
-        remarkCollapse,
-        {
-          test: "Table of contents",
-        },
-      ],
+    site: SITE.website,
+    integrations: [
+        tailwind({
+            applyBaseStyles: false,
+        }),
+        react(),
+        sitemap(),
     ],
-    shikiConfig: {
-      theme: "one-dark-pro",
-      wrap: true,
+    markdown: {
+        remarkPlugins: [
+            remarkToc,
+            [
+                remarkCollapse,
+                {
+                    test: "Table of contents",
+                },
+            ],
+            mermaid
+        ],
+        shikiConfig: {
+            // Choose from Shiki's built-in themes (or add your own)
+            // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+            experimentalThemes: {
+                light: 'material-theme-ocean',
+                dark: 'material-theme-ocean',
+            },
+            wrap: true,
+        },
     },
-  },
-  vite: {
-    optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
+    vite: {
+        ssr: {
+            external: ["svgo"],
+        },
+        optimizeDeps: {
+            exclude: ["@resvg/resvg-js"],
+        },
     },
-  },
-  scopedStyleStrategy: "where",
+    scopedStyleStrategy: "where",
 });
